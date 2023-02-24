59facb16c75bbc5cd7de457d6c234f48c9c48ff9
AlexKabaiev <113040507+AlexKabaiev@users.noreply.github.com>
2023-02-24 09:01:37 +0000

    Deploying to gh-pages from @ AlexKabaiev/goit-js-hw-09@8ff422fa96592aec2ea69d49cc2b0fb29b08b278 🚀

--- 01-color-switcher.0d892675.js
+++
@@ -1,2 +0,0 @@
-let t=null;document.querySelector("[data-start]").addEventListener("click",(function(){const e=document.body;t=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;e.style.backgroundColor=t}),1e3),document.querySelector("[data-start]").disabled=!0,document.querySelector("[data-stop]").disabled=!1})),document.querySelector("[data-stop]").addEventListener("click",(function(){clearInterval(t),document.querySelector("[data-start]").disabled=!1,document.querySelector("[data-stop]").disabled=!0}));
-//# sourceMappingURL=01-color-switcher.0d892675.js.map
---
+++ 01-color-switcher.a04c76f8.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;t.addEventListener("click",(function(){const o=document.body;n=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=t}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.a04c76f8.js.map

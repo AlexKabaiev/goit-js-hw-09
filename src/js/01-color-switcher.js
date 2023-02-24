8ff422fa96592aec2ea69d49cc2b0fb29b08b278
Alexey Kabaiev <kabaievalex@gmail.com>
2023-02-24 11:00:59 +0200

    edit Task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,9 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
 let intervalId = null;
 
 function startBackgroundChange() {
@@ -10,19 +13,15 @@ function startBackgroundChange() {
     const randomColor = getRandomHexColor();
     body.style.backgroundColor = randomColor;
   }, 1000);
-  document.querySelector('[data-start]').disabled = true;
-  document.querySelector('[data-stop]').disabled = false;
+  btnStart.disabled = true;
+  btnStop.disabled = false;
 }
 
 function stopBackgroundChange() {
   clearInterval(intervalId);
-  document.querySelector('[data-start]').disabled = false;
-  document.querySelector('[data-stop]').disabled = true;
+  btnStart.disabled = false;
+  btnStop.disabled = true;
 }
 
-document
-  .querySelector('[data-start]')
-  .addEventListener('click', startBackgroundChange);
-document
-  .querySelector('[data-stop]')
-  .addEventListener('click', stopBackgroundChange);
+btnStart.addEventListener('click', startBackgroundChange);
+btnStop.addEventListener('click', stopBackgroundChange);
